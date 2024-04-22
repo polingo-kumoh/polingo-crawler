1ab0055eef68e2a7fd0df18681e1cfe8e325479b
minturtle <57436755+minturtle@users.noreply.github.com>
2024-04-22 11:07:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import nltk
 from crawler import Crawler
 from article_insertor import ArticleInsertor
+from slack_bot import SlackAPI
+
 import pytz
 import os
 
+slack_token = os.environ["SLACK_TOKEN"]
 
 host = os.environ["MYSQL_HOST"]
 username = os.environ["MYSQL_USERNAME"]
@@ -14,6 +17,8 @@ def main():
     # 최초 실행시 punkt 1번만 다운 필요
     nltk.download('punkt')
 
+    slack_api = SlackAPI(slack_token)
+    
     cnn_url = 'https://edition.cnn.com'
     nhk_url = 'https://www3.nhk.or.jp/news/'
 
@@ -22,15 +27,30 @@ def main():
     article_insertor = ArticleInsertor(host, username, password, database)
 
 
+
+
+    slack_api.post_message(channel_id, f"CRAWLER : News Crawling started")
+
+    
     cnn_articles = filter_and_sort_articles(cnn_crawler.crawl(0, 20))
     nhk_articles = filter_and_sort_articles(nhk_crawler.crawl(0, 20))
 
-    for article in cnn_articles:
-        article["language"] = "ENGLISH"
-        article_insertor.insert(article)
-    for article in nhk_articles:
-        article["language"] = "JAPANESE"
-        article_insertor.insert(article)
+
+    slack_api.post_message(channel_id, f"CRAWLER : News Crawling Completed : english - {len(cnn_articles)}, japanese - {len(nhk_articles)}")
+    slack_api.post_message(channel_id, f"CRAWLER : News Crawling Save Started")
+    try:
+        for article in cnn_articles:
+            article["language"] = "ENGLISH"
+            article_insertor.insert(article)
+        for article in nhk_articles:
+            article["language"] = "JAPANESE"
+            article_insertor.insert(article)
+    except e:
+        slack_api.post_message(channel_id, f"CRAWLER : News Crawl Save Failed")
+    
+    
+    channel_id = slack_api.get_channel_id("polingo-logs")
+    slack_api.post_message(channel_id, f"CRAWLER : News Crawling Save Completed : english - {len(cnn_articles)}, japanese - {len(nhk_articles)}")
 
 def filter_and_sort_articles(articles):
     # publish_date가 None이 아닌 articles만 필터링
