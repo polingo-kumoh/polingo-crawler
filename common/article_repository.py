44e050c21fa799e1de7413e30e1f2e9ae9560b23
minturtle <minseok0425@kakao.com>
2024-04-24 14:34:34 +0900

    Feat : Article Repository 뉴스 조회 기능 추가

--- common/article_repository.py
+++ common/article_repository.py
@@ -56,21 +56,42 @@ class ArticleRepository:
         conn = self.pool.connection()
         try:
             # 커서 생성
-            cursor = conn.cursor()
-            # SQL 쿼리 작성
-            sql = """
-            SELECT news.id, news.title, news.publish_date, news.image_url, news.news_url,
-                   news_sentence.grammars, news_sentence.origin_text, news_sentence.translated_text
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            # 뉴스 기사만 먼저 가져옴
+            sql_news = """
+            SELECT id, title, publish_date, image_url, news_url, language
             FROM news
-            JOIN news_sentence ON news.id = news_sentence.news_id
-            ORDER BY news.publish_date DESC
+            ORDER BY publish_date DESC, id
             LIMIT %s OFFSET %s
             """
-            # SQL 쿼리 실행
-            cursor.execute(sql, (limit, offset))
-            # 결과를 가져와 반환
-            result = cursor.fetchall()
-            return result
+            cursor.execute(sql_news, (limit, offset))
+            news_list = cursor.fetchall()
+
+            # 뉴스 데이터를 dictionary로 구성
+            news_dict = {news['id']: news for news in news_list}
+            for news in news_dict.values():
+                news['sentences'] = []
+
+            # 각 뉴스에 해당하는 모든 문장을 가져옴
+            if news_dict:
+                sql_sentences = """
+                SELECT news_id, id as sentence_id, grammars, origin_text, translated_text
+                FROM news_sentence
+                WHERE news_id IN (%s)
+                """
+                # IN 절에 사용할 뉴스 ID 목록 생성
+                news_ids = list(news_dict.keys())
+                format_strings = ','.join(['%s'] * len(news_ids))
+                cursor.execute(sql_sentences % format_strings, tuple(news_ids))
+                sentences = cursor.fetchall()
+
+                # 문장을 해당 뉴스에 추가
+                for sentence in sentences:
+                    if sentence['news_id'] in news_dict:
+                        news_dict[sentence['news_id']]['sentences'].append(sentence)
+
+            return list(news_dict.values())
+
         except pymysql.Error as e:
             print(f"Error: {e}")
             conn.rollback()
